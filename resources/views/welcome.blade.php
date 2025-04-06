bc62bb49533adfe3d706ba545e239b8b923405fa
John Niyontwali <nijohn0006@gmail.com>
2025-04-06 22:02:06 +0200

    project setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ticket System APIs</title>
+</head>
+<style>
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  body {
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+
+</style>
+<body>
+  <h1>Ticket System APIs</h1>
+</body>
+</html>
